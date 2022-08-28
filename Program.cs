dcd78ba6e42b395ec2f1be2d9ed1a791043d8c42
Huilin-Zhu <65043460+Huilin-Zhu@users.noreply.github.com>
2022-08-28 17:16:43 +0200

    Edit comments.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace Calculator
     {
         public static double DoOperation(double num1, double num2, string op)
         {
-            double result = double.NaN; // This is for default result, if the operation results in an error.
+            // Initiate a new variable. If the operation results in an error, we have the default result defined here.
+            double result = double.NaN;
             switch(op)
             {
                 case "a":
@@ -35,6 +36,7 @@ namespace Calculator
 
     class Program
     {
+        // The Calculator program starts here.
         static void Main(string[] args)
         {
             bool endApp = false;
@@ -43,6 +45,8 @@ namespace Calculator
 
             while (!endApp)
             {
+                // Initiate variables that will be used later in this class. 
+                //Of course we can also initiate them without initial value. But then, if the calculation goes wrong, we will have random error outputs.
                 string numInput1 = "";
                 string numInput2 = "";
                 double result = 0;
@@ -51,6 +55,8 @@ namespace Calculator
                 numInput1 = Console.ReadLine();
 
                 double cleanNum1 = 0;
+                //double.TryParse will turn string numInput1 to a double and store the turned result in double cleanNum1, and return True.
+                // If it cannot turn the string into a double, cleanNum1 will equal to 0, and return False.
                 while (!double.TryParse(numInput1, out cleanNum1))
                 {
                     Console.Write("This is not valid input. Please enter an integer: ");
@@ -63,7 +69,7 @@ namespace Calculator
                 double cleanNum2 = 0;
                 while (!double.TryParse(numInput2, out cleanNum2))
                 {
-                    Console.Write("This is not valid input. Please enter and integer: ");
+                    Console.Write("This is not valid input. Please enter an integer: ");
                     numInput2 = Console.ReadLine();
                 }
 
@@ -79,7 +85,7 @@ namespace Calculator
                 try
                 {
                     result = Calculate.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
+                    if (double.IsNaN(result)) // Means we enter the default case in Calculate Class
                     {
                         Console.WriteLine("This operation will result in NaN.\n");
                     }
@@ -89,7 +95,7 @@ namespace Calculator
                 {
                     Console.WriteLine("Oh no, an exception occurred.\nDetails: " + e.Message);
                 }
-                Console.WriteLine("--------------------------\n");
+                Console.WriteLine("-------------------------\n");
 
                 Console.Write("Press 'n' and Enter to closs the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n")
